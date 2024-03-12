af860d62415c4d256fdf110bec99c42b40207d69
Leonardo Braga <54039202+leopesb@users.noreply.github.com>
2024-03-12 12:23:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HttpClientSample
+{
+    class Program
+    {   
+        private static void Main(string[] args)
+        {
+           GetAsync(client).Wait();
+        }
+
+        private static HttpClient client = new HttpClient()
+        {
+            BaseAddress = new Uri("https://jsonplaceholder.typicode.com"),
+        };
+
+        private static async Task GetAsync (HttpClient httpClient)
+        {
+            using HttpResponseMessage response = await httpClient.GetAsync("todos/3");
+
+            response.EnsureSuccessStatusCode();
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"{jsonResponse}\n");
+        }
+    }
+}
